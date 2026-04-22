347148c6e1848844bb5faf52e79bf46df34385ef
TRẦN TRỌNG PHÚC <trantrongphuca20@gmail.com>
2026-04-23 02:14:51 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,19 @@ const { autoSeedUsers } = require('./controllers/authController');
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// Kết nối Database
 mongoose.connect(MONGO_URI)
   .then(async () => {
     console.log('✅ MongoDB connected');
     try {
-      // Chỉ tạo Admin, không đụng đến sản phẩm
       await autoSeedUsers();
     } catch (err) { console.error('Seed error:', err); }
+  })
+  .catch(err => console.error("❌ MongoDB connection error:", err));
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
-    });
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
   });
+}
+module.exports = app;
